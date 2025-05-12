6dbada9b557760487df79ab244e936e5722b3378
root <preprint@pensoft.net>
2025-05-12 15:37:45 +0300

    new field

---
+++ updates/builder_table_update_pensoft_synergies_data_2.php
@@ -0,0 +1,25 @@
+<?php namespace Pensoft\Synergies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftSynergiesData2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_synergies_data', function($table)
+        {
+            $table->integer('type')->default(1);
+            $table->integer('sort_order')->default(1);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_synergies_data', function($table)
+        {
+            $table->dropColumn('type');
+            $table->dropColumn('sort_order');
+        });
+    }
+}
