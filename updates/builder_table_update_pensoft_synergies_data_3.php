5b0167d8f8295ac240660e647f62001eb1d926a4
root <preprint@pensoft.net>
2025-10-23 17:13:39 +0300

    news field

---
+++ updates/builder_table_update_pensoft_synergies_data_3.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Synergies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftSynergiesData3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_synergies_data', function($table)
+        {
+            $table->string('label')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_synergies_data', function($table)
+        {
+            $table->dropColumn('label');
+        });
+    }
+}
